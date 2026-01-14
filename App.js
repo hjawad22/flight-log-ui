d8a489edd663627fb02eabd64d798f4bf490d5a2
Houda Jawad <houdajawad@gmail.com>
2026-01-14 15:34:14 -0800

    feature: add flight deletion and update UI (error, header, home screen)

--- App.js
+++ App.js
@@ -6,10 +6,9 @@ import FlightForm from './components/FlightForm'
 import FlightHistoryScreen from './components/FlightHistoryScreen'
 import FlightDetailsScreen from './components/FlightDetailsScreen'
 import ErrorComponent from './components/Error'
-import { getUserFlights, postFlight } from './ApiCalls';
+import { getUserFlights, postFlight, deleteFlight} from './ApiCalls';
 
-
-const Stack = createNativeStackNavigator()
+const Stack = createNativeStackNavigator();
 
 class AppNavigator extends Component {
   constructor() {
@@ -17,20 +16,20 @@ class AppNavigator extends Component {
     this.state = {
       userFlights: [],
       error: ''
-    }
+    };
   }
 
   componentDidMount() {
     getUserFlights()
-    .then((data) => this.setState({ userFlights: data }))
-    .catch((error) => {
+     .then((data) => this.setState({ userFlights: data }))
+      .catch((error) => {
        console.error(error);
        this.setState({ error: error.message || 'Unable to add flight. Please try again later.' });
-    });
-}
+      });
+  }
 
   addFlight = (newFlight) => {
-  postFlight(newFlight)
+   postFlight(newFlight)
     .then((flight) => {
       this.setState({ userFlights: [...this.state.userFlights, flight] });
     })
@@ -38,33 +37,66 @@ class AppNavigator extends Component {
       console.error(error);
       this.setState({ error: error.message || 'Could not add your flight, please try again.' });
     });
-};
+  }
+
+  removeFlight = (id, navigation) => {
+    deleteFlight(id)
+      .then(() => {
+        this.setState((prev) => ({ userFlights: prev.userFlights.filter((flight) => flight.id !== id)
+        }));
+        navigation.goBack();
+      })
+      .catch((error) => {
+        this.setState({ error: error.message || 'Could not remove flight, please try again.' });
+      });
+  }
+
 
   render() {
-    const {userFlights, error } = this.state;
-      if (error !== '') {
-      return <ErrorComponent errorMessage={error} />
-    }
+   const { userFlights, error } = this.state;
+    if (error) {
+    return <ErrorComponent errorMessage={error} />;
+   }
 
-    return (
-      <NavigationContainer>
-        <Stack.Navigator initialRouteName="Home">
-          <Stack.Screen name="Home" component={HomeScreen} />
-          <Stack.Screen name="LogFlight" options={{ title: 'Log Flight' }}>
-            {(props) => <FlightForm {...props} addFlight={this.addFlight} userFlights={userFlights} />}
-          </Stack.Screen>
-          <Stack.Screen
-            name="Flight History"
-            component={FlightHistoryScreen}
-            initialParams={{
-              userFlights: this.state.userFlights
-            }} />
-          <Stack.Screen name="FlightDetails" component={FlightDetailsScreen} />
-        </Stack.Navigator>
-      </NavigationContainer>
-    )
+   return (
+   <NavigationContainer>
+   <Stack.Navigator initialRouteName="Home">
+    <Stack.Screen name="Home" component={HomeScreen} />
+    
+    <Stack.Screen name="LogFlight" options={{ title: 'Log Flight' }}>
+        {(props) => (
+          <FlightForm
+            {...props}
+            addFlight={this.addFlight}
+            userFlights={userFlights}
+          />
+        )}
+      </Stack.Screen>
+
+      <Stack.Screen name="Flight History">
+        {(props) => (
+          <FlightHistoryScreen
+            {...props}
+            userFlights={userFlights}
+          />
+        )}
+      </Stack.Screen>
+
+      <Stack.Screen name="FlightDetails">
+        {(props) => (
+          <FlightDetailsScreen
+            {...props}
+            removeFlight={this.removeFlight} 
+          />
+        )}
+      </Stack.Screen>
+
+   </Stack.Navigator>
+   </NavigationContainer>
+   );
+
   }
 }
 
-export default AppNavigator
+export default AppNavigator;
 
